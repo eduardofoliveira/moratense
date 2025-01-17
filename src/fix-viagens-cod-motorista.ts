a56258f60ac68ba09c3ac5ffa720da6139e0f7ba
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2025-01-16 21:42:50 -0300

    Add Stream Transfer

--- src/fix-viagens-cod-motorista.ts
+++ src/fix-viagens-cod-motorista.ts
@@ -1,55 +1,92 @@
 import "dotenv/config"
 
 import Db from "./database/connectionManager"
+import DbH from "./database/connectionManagerHostgator"
 
 const executar = async () => {
   const conn = Db.getConnection()
+  const connHG = DbH.getConnection()
 
-  const [result] = await conn.raw(`
-    SELECT
-			a.id_drank_tel_eventos
-		FROM
-			aux_eventos a
-		where
-			a.event_type_id IN (-3021016551627388635, -8571486384802428876)
-		ORDER BY
-			created_at DESC
-  `)
+  const stream = connHG
+    .select("*")
+    .from("drank_tel_eventos")
+    .whereBetween("data_turno_tel", ["2025-01-08", "2025-01-12"])
+    .stream()
+    .on("finish", () => {
+      console.log("Transferência concluída!")
+    })
 
-  let count = 0
+  // const insertBatch = async (rows: any) => {
+  //   console.log("inserindo")
 
-  for await (const item of result) {
-    console.log(`Evento: ${count++}`)
+  //   // Insere os dados na conexão de destino
+  //   await conn.batchInsert("drank_tel_eventos", rows)
+  // }
 
-    conn.raw(`
-      UPDATE
-        teleconsult.drank_tel_eventos de
-      SET
-        de.id_tipo = 1295
-      where
-        de.id = ${item.id_drank_tel_eventos}
-    `)
+  const batchSize = 500 // Número de registros por batch
+  let batch = []
 
-    conn.raw(`
-      UPDATE
-        teleconsult_api.drank_tel_eventos de
-      SET
-        de.id_tipo = 1295
-      where
-        de.id = ${item.id_drank_tel_eventos}
-    `)
+  // Lê os dados do stream e insere em batches
+  for await (const row of stream) {
+    row.id = undefined
 
-    await conn.raw(`
-      UPDATE
-        teleconsult_app.drank_tel_eventos de
-      SET
-        de.id_tipo = 1295
-      where
-        de.id = ${item.id_drank_tel_eventos}
-    `)
+    batch.push(row)
+    if (batch.length === batchSize) {
+      stream.pause()
+      await conn.batchInsert("drank_tel_eventos", batch)
+      batch = [] // Limpa o batch
+      stream.resume()
+    }
   }
 
-  await conn.destroy()
+  // Insere os registros restantes no batch
+  // if (batch.length > 0) {
+  //   await insertBatch(batch)
+  // }
+
+  // conn.destroy()
+  // connHG.destroy()
+
+  // const conn = Db.getConnection()
+  // const [result] = await conn.raw(`
+  //   SELECT
+  // 		a.id_drank_tel_eventos
+  // 	FROM
+  // 		aux_eventos a
+  // 	where
+  // 		a.event_type_id IN (-3021016551627388635, -8571486384802428876)
+  // 	ORDER BY
+  // 		created_at DESC
+  // `)
+  // let count = 0
+  // for await (const item of result) {
+  //   console.log(`Evento: ${count++}`)
+  //   conn.raw(`
+  //     UPDATE
+  //       teleconsult.drank_tel_eventos de
+  //     SET
+  //       de.id_tipo = 1295
+  //     where
+  //       de.id = ${item.id_drank_tel_eventos}
+  //   `)
+  //   conn.raw(`
+  //     UPDATE
+  //       teleconsult_api.drank_tel_eventos de
+  //     SET
+  //       de.id_tipo = 1295
+  //     where
+  //       de.id = ${item.id_drank_tel_eventos}
+  //   `)
+  //   await conn.raw(`
+  //     UPDATE
+  //       teleconsult_app.drank_tel_eventos de
+  //     SET
+  //       de.id_tipo = 1295
+  //     where
+  //       de.id = ${item.id_drank_tel_eventos}
+  //   `)
+  // }
+  // await conn.destroy()
 }
 
 executar()
