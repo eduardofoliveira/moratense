b41ef8bac069217ded8bb7ea75a7740a6b24394c
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2025-02-08 14:51:37 -0300

    Add Test Get Data Globus

---
+++ src/syncGlobus.ts
@@ -0,0 +1,44 @@
+import "dotenv/config"
+
+import Db from "./database/connectionManagerOracle"
+
+const execute = async () => {
+  const start = "2025-02-05 00:00:00"
+  const end = "2025-02-05 23:59:59"
+
+  const db = Db.getConnection()
+  const data = await db.raw(`
+    select
+      to_char( t_arr_viagens_guia.QTD_HORA_INI , 'yyyy-mm-dd' ) dt,
+      prefixoveic,
+      l.codigolinha,
+      l.nomelinha,
+      codigoorgconc,
+		  '' cod_servdiaria,
+      to_char( t_arr_viagens_guia.QTD_HORA_INI , 'yyyy-mm-dd hh24:mi:ss' ) dti,
+      to_char( t_arr_viagens_guia.QTD_HORA_FINAL , 'yyyy-mm-dd hh24:mi:ss' ) dtf,
+		  frt_cadveiculos.CODIGOTPFROTA cdft,
+      fm.codfunc f1cod,
+      fm.apelidofunc f1ap,
+      fm.nomefunc f1nome,
+      to_char( t_arr_viagens_guia.QTD_HORA_INI , 'yyyy-mm-dd hh24:mi:ss' ) dtg
+		from
+      t_arr_viagens_guia
+      left join frt_cadveiculos
+        on frt_cadveiculos.codigoveic = t_arr_viagens_guia.COD_VEICULO
+			Join bgm_cadlinhas l
+        on t_arr_viagens_guia.cod_intlinha = l.codintlinha
+      left join T_ARR_TROCAS_FUNC
+        on T_ARR_TROCAS_FUNC.COD_SEQ_GUIA = t_arr_viagens_guia.COD_SEQ_GUIA and FLG_MOT_COB = 'M'
+			left Join flp_funcionarios fm
+        on fm.CODINTFUNC = T_ARR_TROCAS_FUNC.CODINTFUNC
+		where
+      t_arr_viagens_guia.QTD_HORA_INI between to_date(${start} || ' 03:00:00','yyyy-mm-dd hh24:mi:ss') and to_date(${end} || ' 02:59:59','yyyy-mm-dd hh24:mi:ss')
+  `)
+
+  console.log(data)
+
+  await db.destroy()
+}
+
+execute()
