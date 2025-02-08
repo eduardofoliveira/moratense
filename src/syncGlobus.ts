34e66876471011fc2910d91b8686fa9beeda32ac
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2025-02-08 15:39:51 -0300

    Test SID Connect

--- src/syncGlobus.ts
+++ src/syncGlobus.ts
@@ -7,36 +7,36 @@ const execute = async () => {
   const end = "2025-02-05 23:59:59"
 
   const db = Db.getConnection()
-  const data = await db.raw(`
-    select
-      to_char( t_arr_viagens_guia.QTD_HORA_INI , 'yyyy-mm-dd' ) dt,
-      prefixoveic,
-      l.codigolinha,
-      l.nomelinha,
-      codigoorgconc,
-		  '' cod_servdiaria,
-      to_char( t_arr_viagens_guia.QTD_HORA_INI , 'yyyy-mm-dd hh24:mi:ss' ) dti,
-      to_char( t_arr_viagens_guia.QTD_HORA_FINAL , 'yyyy-mm-dd hh24:mi:ss' ) dtf,
-		  frt_cadveiculos.CODIGOTPFROTA cdft,
-      fm.codfunc f1cod,
-      fm.apelidofunc f1ap,
-      fm.nomefunc f1nome,
-      to_char( t_arr_viagens_guia.QTD_HORA_INI , 'yyyy-mm-dd hh24:mi:ss' ) dtg
-		from
-      t_arr_viagens_guia
-      left join frt_cadveiculos
-        on frt_cadveiculos.codigoveic = t_arr_viagens_guia.COD_VEICULO
-			Join bgm_cadlinhas l
-        on t_arr_viagens_guia.cod_intlinha = l.codintlinha
-      left join T_ARR_TROCAS_FUNC
-        on T_ARR_TROCAS_FUNC.COD_SEQ_GUIA = t_arr_viagens_guia.COD_SEQ_GUIA and FLG_MOT_COB = 'M'
-			left Join flp_funcionarios fm
-        on fm.CODINTFUNC = T_ARR_TROCAS_FUNC.CODINTFUNC
-		where
-      t_arr_viagens_guia.QTD_HORA_INI between to_date('${start}','yyyy-mm-dd hh24:mi:ss') and to_date('${end}','yyyy-mm-dd hh24:mi:ss')
-  `)
+  // const data = await db.raw(`
+  //   select
+  //     to_char( t_arr_viagens_guia.QTD_HORA_INI , 'yyyy-mm-dd' ) dt,
+  //     prefixoveic,
+  //     l.codigolinha,
+  //     l.nomelinha,
+  //     codigoorgconc,
+  // 	  '' cod_servdiaria,
+  //     to_char( t_arr_viagens_guia.QTD_HORA_INI , 'yyyy-mm-dd hh24:mi:ss' ) dti,
+  //     to_char( t_arr_viagens_guia.QTD_HORA_FINAL , 'yyyy-mm-dd hh24:mi:ss' ) dtf,
+  // 	  frt_cadveiculos.CODIGOTPFROTA cdft,
+  //     fm.codfunc f1cod,
+  //     fm.apelidofunc f1ap,
+  //     fm.nomefunc f1nome,
+  //     to_char( t_arr_viagens_guia.QTD_HORA_INI , 'yyyy-mm-dd hh24:mi:ss' ) dtg
+  // 	from
+  //     t_arr_viagens_guia
+  //     left join frt_cadveiculos
+  //       on frt_cadveiculos.codigoveic = t_arr_viagens_guia.COD_VEICULO
+  // 		Join bgm_cadlinhas l
+  //       on t_arr_viagens_guia.cod_intlinha = l.codintlinha
+  //     left join T_ARR_TROCAS_FUNC
+  //       on T_ARR_TROCAS_FUNC.COD_SEQ_GUIA = t_arr_viagens_guia.COD_SEQ_GUIA and FLG_MOT_COB = 'M'
+  // 		left Join flp_funcionarios fm
+  //       on fm.CODINTFUNC = T_ARR_TROCAS_FUNC.CODINTFUNC
+  // 	where
+  //     t_arr_viagens_guia.QTD_HORA_INI between to_date('${start}','yyyy-mm-dd hh24:mi:ss') and to_date('${end}','yyyy-mm-dd hh24:mi:ss')
+  // `)
 
-  console.log(data)
+  // console.log(data)
 
   await db.destroy()
 }
