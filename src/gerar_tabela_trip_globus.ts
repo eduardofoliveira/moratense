0f179290460ca357757b87c64f2b147d37ac3699
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2025-03-30 18:20:38 -0300

    Ignore unknow driver

--- src/gerar_tabela_trip_globus.ts
+++ src/gerar_tabela_trip_globus.ts
@@ -113,6 +113,10 @@ const gerarIndicadores = async () => {
           t.tripStart BETWEEN '${inicio} 03:00:00' AND '${termino} 02:59:59'
       `)
 
+      if (trip.driverId === "-9110386254540308778") {
+        continue
+      }
+
       const [resumoEventos] = await connMoratense.raw(`
         SELECT
           ec.descricao_exibida,
