8cb2d263a2f6e1185bba314fda9e92a72c925e38
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2025-04-03 20:21:58 -0300

    Test Crontab

--- src/crontab.ts
+++ src/crontab.ts
@@ -24,19 +24,21 @@ function executarComando(comando: any, callback: any) {
 const executarRotina = async () => {
   console.log("Executando tarefa programada todos os dias às 03:00")
 
-  executarComando(path.resolve("./dist/syncGlobus.js"), () => {
+  executarComando(`node ${path.resolve("./dist/syncGlobus.js")}`, () => {
     console.log(new Date().toLocaleString())
-    console.log(path.resolve("./dist/syncGlobus.js"))
+    console.log(`node ${path.resolve("./dist/syncGlobus.js")}`)
 
-    executarComando(path.resolve("./dist/vinculo.js"), () => {
+    executarComando(`node ${path.resolve("./dist/vinculo.js")}`, () => {
       console.log(new Date().toLocaleString())
-      console.log(path.resolve("./dist/vinculo.js"))
+      console.log(`node ${path.resolve("./dist/vinculo.js")}`)
 
       executarComando(
-        path.resolve("./dist/gerar_tabela_trip_globus.js"),
+        `node ${path.resolve("./dist/gerar_tabela_trip_globus.js")}`,
         () => {
           console.log(new Date().toLocaleString())
-          console.log(path.resolve("./dist/gerar_tabela_trip_globus.js"))
+          console.log(
+            `node ${path.resolve("./dist/gerar_tabela_trip_globus.js")}`,
+          )
         },
       )
     })
