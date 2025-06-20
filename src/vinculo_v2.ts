5467e2a0d326be92f2372a74962595d1c220ebcc
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2025-06-20 06:16:42 -0300

    Vinculo Dates

--- src/vinculo_v2.ts
+++ src/vinculo_v2.ts
@@ -266,7 +266,7 @@ const executar = async () => {
   // const inicio = format(startOfDay(subDays(today, 2)), "yyyy-MM-dd")
   // const termino = format(endOfDay(subDays(today, 1)), "yyyy-MM-dd")
 
-  const inicio = "2025-06-15"
+  const inicio = "2025-06-14"
   const termino = "2025-06-18"
 
   let start = inicio
