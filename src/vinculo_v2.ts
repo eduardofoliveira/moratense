775bdba37c3d6cdd0ccbec2bacd2826179dc4618
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2025-06-20 07:01:41 -0300

    Sync Globus

--- src/vinculo_v2.ts
+++ src/vinculo_v2.ts
@@ -262,12 +262,12 @@ const vinculoPorMotorista = async ({
 const executar = async () => {
   const connMoratense = DbMoratense.getConnection()
 
-  // const today = new Date()
-  // const inicio = format(startOfDay(subDays(today, 2)), "yyyy-MM-dd")
-  // const termino = format(endOfDay(subDays(today, 1)), "yyyy-MM-dd")
+  const today = new Date()
+  const inicio = format(startOfDay(subDays(today, 2)), "yyyy-MM-dd")
+  const termino = format(endOfDay(subDays(today, 1)), "yyyy-MM-dd")
 
-  const inicio = "2025-06-13"
-  const termino = "2025-06-18"
+  // const inicio = "2025-06-13"
+  // const termino = "2025-06-18"
 
   let start = inicio
   let endDate = format(
