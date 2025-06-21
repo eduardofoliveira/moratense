3e52ca93ff9ad1aacf24666448d9e92a7d1df8f7
Eduardo F Oliveira <eduardo_felipe_oliveira@yahoo.com.br>
2025-06-21 02:20:56 -0300

    Processar Semana

--- src/process-follow-up-cross-reference.ts
+++ src/process-follow-up-cross-reference.ts
@@ -459,12 +459,12 @@ const gerarIndicadores = async (
 }
 
 const execute = async () => {
-  // const hoje = new Date()
-  // const start = format(subDays(hoje, 8), "yyyy-MM-dd 03:00:00")
-  // const end = format(subDays(hoje, 1), "yyyy-MM-dd 02:59:59")
+  const hoje = new Date()
+  const start = format(subDays(hoje, 8), "yyyy-MM-dd 03:00:00")
+  const end = format(subDays(hoje, 1), "yyyy-MM-dd 02:59:59")
 
-  const start = "2025-06-09 03:00:00"
-  const end = "2025-06-16 02:59:59"
+  // const start = "2025-06-09 03:00:00"
+  // const end = "2025-06-16 02:59:59"
 
   await gerarIndicadores(start, end, 2) // In loco
   await gerarIndicadores(start, end, 1) // Orientação
--- src/process-follow-up-nao-monitorados.ts
+++ src/process-follow-up-nao-monitorados.ts
@@ -416,12 +416,12 @@ const gerarIndicadores = async ({ start, end }: InputParams) => {
 }
 
 const execute = async () => {
-  // const hoje = new Date()
-  // const start = format(subDays(hoje, 8), "yyyy-MM-dd 03:00:00")
-  // const end = format(subDays(hoje, 1), "yyyy-MM-dd 02:59:59")
+  const hoje = new Date()
+  const start = format(subDays(hoje, 8), "yyyy-MM-dd 03:00:00")
+  const end = format(subDays(hoje, 1), "yyyy-MM-dd 02:59:59")
 
-  const start = "2025-06-09 03:00:00"
-  const end = "2025-06-16 02:59:59"
+  // const start = "2025-06-09 03:00:00"
+  // const end = "2025-06-16 02:59:59"
 
   await gerarIndicadores({
     start,
